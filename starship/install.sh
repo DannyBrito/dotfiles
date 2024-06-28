799a8214bfcaa8890283a00c999bfa7c9067bc46
Danny Brito <54994380+DannyBrito@users.noreply.github.com>
2024-06-28 17:37:08 -0400

    add support for no-font starship in default apple terminal

--- starship/install.sh
+++ starship/install.sh
@@ -8,6 +8,7 @@ curl -sS https://starship.rs/install.sh | sh
 mkdir -p $HOME/.config
 mv $HOME/.config/starship.toml $HOME/.config/starship.old.toml || true
 ln -s $PWD/starship/starship.toml $HOME/.config/starship.toml
+ln -s $PWD/starship/starship.no-font.toml $HOME/.config/starship.no-font.toml
 
 os_shell="bash"
 os_shell_file=".bashrc"
