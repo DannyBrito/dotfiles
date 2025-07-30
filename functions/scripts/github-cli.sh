7e6f20e5578bde977263d2bb8c6fc8eb89b3c3a6
Danny Brito <dannybrito@microsoft.com>
2025-07-30 20:25:22 +0000

    fix

--- functions/scripts/github-cli.sh
+++ functions/scripts/github-cli.sh
@@ -4,7 +4,7 @@ function git-download-artifacts(){
     local run_id="$(echo "$1" | grep -oP '(?<=runs/)\d+')"
     local outdir="${2}"
     if [[ -z "$outdir" ]]; then
-        outdir="${run_id}-artifacts"
+        outdir="gh-artifacts-${run_id}"
     fi
     cmd="gh run download "${run_id}" -D "${outdir}""
     echo "running: $cmd"
